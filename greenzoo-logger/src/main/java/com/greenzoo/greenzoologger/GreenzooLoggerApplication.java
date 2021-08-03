dbfdc09013017a032aca0fa1549f497df2ec4cef
greenzoo-quokka <greenzoo.quokka@gmail.com>
2021-08-03 23:06:01 +0900

    init greenzoo-logger project

---
+++ greenzoo-logger/src/main/java/com/greenzoo/greenzoologger/GreenzooLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.greenzoo.greenzoologger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenzooLoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenzooLoggerApplication.class, args);
+    }
+
+}
---
+++ greenzoo-logger/src/test/java/com/greenzoo/greenzoologger/GreenzooLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenzoo.greenzoologger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenzooLoggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
