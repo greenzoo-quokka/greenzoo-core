32cf0a295921ad321736f6d06db36c44dbca30b8
greenzoo-quokka <greenzoo.quokka@gmail.com>
2021-08-04 19:02:43 +0900

    init greenzoo-actuator project

---
+++ greenzoo-actuator/src/main/java/com/greenzoo/actuator/greenzooactuator/GreenzooActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.greenzoo.actuator.greenzooactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenzooActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenzooActuatorApplication.class, args);
+    }
+
+}
---
+++ greenzoo-actuator/src/test/java/com/greenzoo/actuator/greenzooactuator/GreenzooActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenzoo.actuator.greenzooactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenzooActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
