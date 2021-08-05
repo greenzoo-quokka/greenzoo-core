8fdb4dba5a818b81d2a434d7528496291c87ae5d
greenzoo-quokka <greenzoo.quokka@gmail.com>
2021-08-05 23:13:07 +0900

    init greenzoo-doc project

---
+++ greenzoo-doc/src/main/java/com/greenzoo/doc/greenzoodoc/GreenzooDocApplication.java
@@ -0,0 +1,13 @@
+package com.greenzoo.doc.greenzoodoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenzooDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenzooDocApplication.class, args);
+    }
+
+}
---
+++ greenzoo-doc/src/test/java/com/greenzoo/doc/greenzoodoc/GreenzooDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenzoo.doc.greenzoodoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenzooDocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
